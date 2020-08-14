5b9a9d9a4bdc3c7c44bdbbe09fd2e15b036c5fcf
M3ANMAXINE <CHERRERAJ@HOTMAIL.COM>
2020-08-13 23:10:56 -0400

    creacion de archivo vue-cli con router

---
+++ vue-cli/src/views/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Vuelve a intentarlo bitch</h1>
+    <p>{{ route }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    route () {
+      return this.$route.path
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
